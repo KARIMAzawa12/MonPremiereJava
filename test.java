32791e9adf320fc45f1b6b99e4388ccad7f4bf52
KARIMAzawa12 <123784619+KARIMAzawa12@users.noreply.github.com>
2023-10-18 18:10:17 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello github");
+        System.out.println("hello githublkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
     }
     
 }
